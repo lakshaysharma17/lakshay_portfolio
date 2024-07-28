5657b88250b5227ebf599fe4187ae5d6fda9255c
Lakshay Sharma <143041033+lakshaysharma17@users.noreply.github.com>
2024-07-28 16:02:29 +0530

    Update About-me.jsx

--- src/components/About-me.jsx
+++ src/components/About-me.jsx
@@ -1,7 +1,7 @@
 export function About() {
     return (
       <div className="flex justify-center items-center">
-        <img className="h-48 pl-28 pr-10 rounded" alt="" src="/public/profile.png" />
+        <img className="h-48 pl-28 pr-10 rounded" alt="" src="/profile.jpeg" />
         <p className="text-gray-200 text-left  ">Hi there! I'm Lakshay Sharma, currently in my pre-final year at Bhagwan Parshuram Institute of Technology (BPIT), affiliated with Indraprastha University (GGSIPU). I'm pursuing B.Tech in Electronics and Communication Engineering (ECE). I am well-versed in C++ and have a strong foundation in DSA and OOPS. I'm passionate about leveraging technology to solve real-world problems and am always eager to learn and explore new opportunities.. Talented Software Developer equipped with great coding, debugging and project management abilities. Accomplishing project goals consistently with elegant, scalable code.
 </p>
       </div>
